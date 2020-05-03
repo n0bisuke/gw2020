bfd0c677400f22c52d117e471c174b8701f2b2f0
n0bisuke <rsksound@gmail.com>
2020-05-04 02:15:33 +0900

    01-08 update

--- actionfox.js
+++ actionfox.js
@@ -8,7 +8,7 @@ const LINE_NOTIFY_API_URL = 'https://notify-api.line.me/api/notify';
 // 実際には、GitHub Actions の
 // run: LINE_TOKEN=${{secrets.LINE_TOKEN}} node action.js
 // という書き方で渡されています
-const LINE_NOTIFY_TOKEN = process.env.LINE_TOKEN || 'とーくん';
+const LINE_NOTIFY_TOKEN = process.env.LINE_TOKEN || 'mZ20574N36I7ME0HrgZMP1AOAjplTqoL7fb6PghVQQD';
 
 let config = {
     url: LINE_NOTIFY_API_URL,
@@ -17,29 +17,23 @@ let config = {
         'Content-Type': 'application/x-www-form-urlencoded',
         'Authorization': 'Bearer ' + LINE_NOTIFY_TOKEN
     },
-    data: qs.stringify({
-      imageFullsize: `https://images.dog.ceo/breeds/airedale/n02096051_3443.jpg`,
-      imageThumbnail: `https://images.dog.ceo/breeds/airedale/n02096051_3443.jpg`,
-      message: 'ProtoOut Studioからの通知だよー！ https://images.dog.ceo/breeds/airedale/n02096051_3443.jpg',
-    })
+    data: ''
 }
 
 async function getRequest() {
 
   ////// LINE Notify に送る ////////////////////////
-
+  let foxResponse;
   try {
-    const foxResponse = await axios.get('https://randomfox.ca/floof/');
-    // console.log(foxResponse.data.image);
-    const foxImage = foxResponse.data.image;
-    config.data = qs.stringify({
-      imageFullsize: foxImage,
-      imageThumbnail: foxImage,
-      message: 'ProtoOut Studioからの通知だよー！',
-    })
-
-    const responseLINENotify = await axios.request(config); //LINE Notifyに送る
+    foxResponse = await axios.get(`https://randomfox.ca/floof/`);
+    console.log(foxResponse.data.image);
+    config.data = {
+        message: 'ProtoOut Studioからの通知だよー！GitHub Actionsだよー！',
+        imageThumbnail: foxResponse.data.image,
+        imageFullsize: foxResponse.data.image,
+    }
     console.log(responseLINENotify.data);
+    const responseLINENotify = await axios.request(config);
   } catch (error) {
     console.error(error);
   }
---
+++ datasync.js
@@ -0,0 +1,26 @@
+'use strcit';
+
+const axios = require('axios');
+const fs = require("fs"); //注: npm i 不要
+
+//データ更新関数
+async function updateData(newData){
+    const PATH = "./docs/data.json";
+    fs.writeFileSync(PATH, JSON.stringify(newData));
+}
+
+// 実際にデータを取得する getRequest 関数
+async function getRequest() {
+  let response;
+  try {
+    response = await axios.get('http://api.open-notify.org/iss-pass.json?lat=45.0&lon=-122.3&alt=20&n=5');
+    console.log(response.data);
+
+    await updateData(response.data); //データ更新関数を実行
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// getRequest を呼び出してデータを読み込む
+getRequest();
